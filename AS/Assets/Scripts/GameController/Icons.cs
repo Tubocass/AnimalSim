b5f02d9409c9abf696a6444173632a17f20cee5a
Tubocass <tubocass@gmail.com>
2014-06-12 00:37:02 -0500

    Probably nothing changed, I'm just updating master up a couple commits.

--- AS/Assets/Scripts/GameController/Icons.cs
+++ AS/Assets/Scripts/GameController/Icons.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Icons : MonoBehaviour {
+public class Icons : MonoBehaviour 
+{
 
 	public GUITexture howl;
+	public GUITexture sniff;
 	public GUITexture bite;
 	public GUITexture grab;
 	public GUITexture eat;
--- AS/Assets/Scripts/Player/Select.cs
+++ AS/Assets/Scripts/Player/Select.cs
@@ -3,17 +3,26 @@ using System.Collections;
 
 public class Select : MonoBehaviour {
 public RaycastHit hit;
+public GUITexture crosshair;
+Icons icons;
 
 
-	
-	// Update is called once per frame
-	void Update () {
+void Start()
+{
+	icons = new Icons();
+}
+
+// Update is called once per frame
+void Update () 
+{
 		Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2, 0));
 
-		if(Physics.Raycast (ray, out hit, 10)){
+		if(Physics.Raycast (ray, out hit, 10))
+		{
 			if(hit.collider.gameObject != null)
 			{
-				//GetComponent<GUITexture>().texture = images.
+				if(hit.collider.gameObject == GameObject.FindGameObjectWithTag("Shit"))
+				crosshair = icons.sniff;
 
 				if(Input.GetButtonDown("Fire1"))
 				{
@@ -21,6 +30,6 @@ public RaycastHit hit;
 					hit.collider.gameObject.GetComponent<interact>().isSeen();
 				}else Debug.Log("Gnip");
 			}
-	}
+		}
 }
 }
