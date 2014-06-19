c365533d51f722bdb66b227b1cf16c7ccabc9c9b
BoB <tubocass@gmail.com>
2014-06-18 19:05:43 -0500

    Added food object that gets destroyed.

--- AS/Assets/Scripts/Player/Select.cs
+++ AS/Assets/Scripts/Player/Select.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class Select : MonoBehaviour {
 public RaycastHit hit;
 
-Texture texture;
 Icons icons;
 
 
@@ -18,22 +17,35 @@ void Start()
 void Update () 
 {
 		Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2, 0));
-
+		GetComponent<GUITexture>().texture = icons.none;
 		if(Physics.Raycast (ray, out hit, 10))
 		{
 			if(hit.collider.gameObject != null)
 			{
-				if(hit.collider.gameObject == GameObject.FindGameObjectWithTag("Shit"))
+				switch(hit.collider.gameObject.tag)
 				{
-					Debug.Log("Ping");
-					GetComponent<GUITexture>().texture = icons.sniff;
+					//Debug.Log("Ping");
+					case  "Shit":
+						GetComponent<GUITexture>().texture = icons.sniff;
 
-					if(Input.GetButtonDown("Fire1"))
-					{
+						if(Input.GetButtonDown("Fire1"))
+						{
 
-						hit.collider.gameObject.GetComponent<interact>().isSeen();
-					}
-				}else GetComponent<GUITexture>().texture = icons.none;
+							hit.collider.gameObject.GetComponent<interact>().isSeen();
+						}
+						break;
+					case "Food":
+						GetComponent<GUITexture>().texture = icons.eat;
+						if(Input.GetButtonDown("Fire1"))
+						{
+							Destroy(hit.collider.gameObject,3);
+							/*
+							 * destroy object
+							 * decrease hunger
+							*/
+						}
+					break;
+				}
 			} 
 		}
 }
