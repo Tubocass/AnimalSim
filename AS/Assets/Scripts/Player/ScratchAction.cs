99545ef305e80f84faf1a0e8cf1bc2ab498d9e36
Jarred Douthit <jdohgamer@gmail.com>
2014-06-25 16:33:29 -0500

    Howl Action/ Piss Action (H and P key press)

---
+++ AS/Assets/Scripts/Player/ScratchAction.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class ScratchAction : MonoBehaviour
+
+{
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+
+}
+}
