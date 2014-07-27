baeab200f6d5d74e0cd8f7b7b5ef237c4c098f20
Jarred Douthit <jdohgamer@gmail.com>
2014-07-26 22:20:37 -0500

    Progress made with NPC Animations. Added Movement script. Switching gears to Player Health tracker

---
+++ AS/Assets/Movement.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(CharacterController))]
+public class Movement : MonoBehaviour {
+	public CollisionFlags collision;
+	public float moveSpeed = 4;
+	public float jumpSpeed = 4;
+	public float turnSpeed = 4;
+	public float gravity = 10;
+	public Vector3 moveDirection;
+	public CharacterController herd;
+	//public GameObject go;
+//	public Transform nPC;
+
+	private AnimationState idle;
+	private AnimationState walk;
+	private AnimationState run;
+	// Use this for initialization
+	void Start () {
+		herd = GetComponent<CharacterController> ();
+		moveDirection = Vector3.zero;
+		print ("step1");
+		animation.wrapMode = WrapMode.Loop;
+		animation ["Big_Cat_Idle"].wrapMode = WrapMode.Once;
+		animation ["Big_Cat_Idle"].layer = 1;
+
+		idle = animation ["Big_Cat_Idle"];
+		walk = animation ["Big_Cat_Walk"];
+		run = animation ["Big_Cat_Walk"];
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AIPath speedcheck = new AIPath();
+
+	//	nPC = GameObject.FindGameObjectWithTag(Tags.food).transform;
+	//	go.transform.LookAt (nPC);
+				animation.CrossFade ("Big_Cat_Walk");
+		print ("alwayson");
+				if (Input.GetAxis ("Horizontal") != 0) {
+						transform.Rotate (0, Input.GetAxis ("Horizontal") / turnSpeed, 0);
+			print ("ok");
+				}
+		if (speedcheck.speed > 0) {
+						animation.Play ("Big_Cat_Walk");
+				}
+		if (speedcheck.speed == 0){
+					animation.Play("Big_Cat_Idle");
+		}
+				
+
+
+				moveDirection.y -= gravity * Time.deltaTime;
+
+				collision = herd.Move (moveDirection * Time.deltaTime);
+
+	if (Input.GetAxis("Vertical") != 0){
+		animation.Play("Big_Cat_Walk");
+			print ("ok2");
+	}
+
+
+
+
+
+
+}
+}
--- AS/Assets/Scripts/NPCs/AstarAI.cs
+++ AS/Assets/Scripts/NPCs/AstarAI.cs
@@ -14,7 +14,7 @@ public class AstarAI : MonoBehaviour {
     public Path path;
     
     //The AI's speed per second
-    public float speed = 100;
+    public float speed = 500;
     
     //The max distance from the AI to a waypoint for it to continue to the next waypoint
     public float nextWaypointDistance = 3;
